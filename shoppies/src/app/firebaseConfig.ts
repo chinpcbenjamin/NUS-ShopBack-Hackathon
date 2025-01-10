26a0d7f06be7502faf5a6845bcca63ceed61f8d0
chinpcbenjamin <chinpcbenjamin@gmail.com>
2025-01-10 23:56:40 +0800

    added-createUser

--- shoppies/src/app/firebaseConfig.ts
+++ shoppies/src/app/firebaseConfig.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { collection, addDoc, getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCNs0dmj_xnh1BTd7F-31nZVH385mIYAK4",
@@ -10,3 +11,17 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig)
+export const database = getFirestore(app)
+
+export async function createUser(username : string) {
+    try {
+        const response = await addDoc(collection(database, "users"), {
+            "username" : username,
+            "streak" : [],
+            "purchases" : [],
+            "missions" : []
+        })
+    } catch (error) {
+        console.log("Error", error)
+    }
+}
