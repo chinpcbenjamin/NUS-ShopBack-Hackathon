3df8043b065792c1e24968ef2d9dc1eff61577a3
Haris-Irfan <harisirfan@gmail.com>
2025-01-12 18:03:51 +0800

    Simplify navigation

--- shoppies/src/app/layout.tsx
+++ shoppies/src/app/layout.tsx
@@ -1,42 +1,15 @@
 "use client";
 
 import React from "react";
-import { useRouter } from "next/navigation";
 
 export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-
   return (
     <html lang="en">
       <body>
-        <header className="bg-gray-800 text-white p-4">
-          <nav className="flex justify-around">
-            <button
-              className="text-white hover:text-gray-300"
-              onClick={() => router.push("/")}
-            >
-              Home
-            </button>
-            <button
-              className="text-white hover:text-gray-300"
-              onClick={() => router.push("/rewards")}
-            >
-              Rewards
-            </button>
-            <button
-              className="text-white hover:text-gray-300"
-              onClick={() => router.push("/streak")}
-            >
-              Streak
-            </button>
-          </nav>
-        </header>
-
-        {/* Main Content */}
         <main>{children}</main>
       </body>
     </html>
